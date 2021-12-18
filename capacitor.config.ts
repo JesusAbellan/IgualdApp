e54e034cde08aa4caca308aad20bd2037144e48c
JesusAbellan <jesusabellan97@gmail.com>
2021-12-18 13:28:12 +0100

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'IgualdApp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
