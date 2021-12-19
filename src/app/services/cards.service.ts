6c2ac160893442e4cbeb3354464a3efc9b684f66
Kira <kira.mironova.external@atos.net>
2021-12-19 10:40:21 +0100

    terminada homepage funcional

---
+++ src/app/services/cards.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Card } from '../interfaces/card';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CardsService {
+
+  constructor(private _http:HttpClient) { }
+
+  getCards() {
+    return new Promise<Card[]>((resolve, reject) => {
+      const url = '../../assets/cards.json';
+      let cards: Card[] = [];
+      this._http.get<Card[]>(url).subscribe(cards=>{
+        resolve(cards);
+      }, err=>{
+        console.log('Hay un error trayendo cards');
+        reject(err);
+      });
+    });
+  }
+}
