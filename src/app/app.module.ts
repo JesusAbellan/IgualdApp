52abe311a4f400b761745ac88cfc8f0a9cf4ad15
Kira <kira.mironova.external@atos.net>
2022-02-17 06:55:17 +0100

    first part

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ComponentsModule } from './components/components.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, ComponentsModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, ComponentsModule, FormsModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/pages/registro/registro.page.ts
+++ src/app/pages/registro/registro.page.ts
@@ -1,3 +1,4 @@
+import { Usuario } from './../../interfaces/usuario';
 import { Component, OnInit } from '@angular/core';
 import { Centro } from 'src/app/interfaces/centro';
 import { CentrosService } from 'src/app/services/centros.service';
@@ -8,16 +9,46 @@ import { CentrosService } from 'src/app/services/centros.service';
   styleUrls: ['./registro.page.scss'],
 })
 export class RegistroPage implements OnInit {
+  public usuario = {
+    nombre:'',
+    apellidos:'',
+    email:'',
+    role:[],
+    pwd:''
+  };
+  public pwd2 = '';
+  public cadenaError = '';
   centros:Centro[] = [];
   constructor(
               private _centrosService: CentrosService) { }
-
+ss
   async ngOnInit() {
     try {
       this.centros = (await this._centrosService.getCentros());
     } catch (error) {
       console.log(error);
     }
+  }
+
+  register() {
+
+  }
+
+  esValido():boolean{
+    if (this.usuario.nombre.length<2) {
+      this.cadenaError = 'El nombre debe tener al menos 2 caracteres';
+    } else {
+      if (this.usuario.pwd.length<6) {
+        this.cadenaError = 'La contraseña debe tener al menos 6 caracteres';
+      } else {
+        if (this.usuario.pwd != this.pwd2) {
+          this.cadenaError = 'Las contraseñas no coinciden. Inténtalo otra vez';          
+        } else {
+          return true;
+        }
+      }
+    }
+    return false; //comprobaciones
   }
 
 }
